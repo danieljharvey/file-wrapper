2318e59304cb390594a8f61c5dfae8d690e1c25f
Daniel Harvey <danieljamesharvey@gmail.com>
2017-04-19 13:20:33 +0100

    More PHP functions, alphabetise

--- src/FileWrapper.php
+++ src/FileWrapper.php
@@ -4,10 +4,40 @@ namespace DanielJHarvey\FileWrapper;
 
 // doesn't do anything clever, just means that file actions can be stubbed in testing (so we can mock responses and not write all over test machine)
 
+// also have changed function_names_like_this to camelCasedFunctions instead because I prefer the way they look. Fight me.
+
 class FileWrapper {
+		
+	public function basename($path,$suffix=false) {
+		return basename($path,$suffix);
+	}
+
+	public function chgrp($filename,$group) {
+		return chgrp($filename,$group);
+	}
+
+	public function chown($filename,$user) {
+		return chown($filename,$user);
+	}
+
+	public function copy($source, $dest, $context=false) {
+		return copy($source,$dest,$context);	
+	}
+
+	public function chmod($filename,$mode) {
+		return chmod($filename,$mode);
+	}
+
+	public function dirName($path,$levels=1) {
+		return dirname ($path, $levels);
+	}
+
+	public function diskFreeSpace($directory) {
+		return disk_free_space($directory);	
+	}
 	
-	public function realPath($path) {
-		return realpath($path);
+	public function diskTotalSpace($directory) {
+		return disk_total_space($directory);
 	}
 
 	public function fileExists($filename) {
@@ -22,8 +52,16 @@ class FileWrapper {
 		return file_put_contents($filename,$data);
 	}
 
+	public function moveUploadedFile($filename, $destination) {
+		return move_uploaded_file($filename,$destination);
+	}
+
 	public function umask($mask) {
 		return umask($mask);
+	}
+
+	public function unlink($filename,$context=false) {
+		return unlink($filename,$context);
 	}
 
 	public function mkdir($pathName, $mode= 0777, $recursive=false) {
@@ -36,6 +74,14 @@ class FileWrapper {
 	
 	public function pathInfo($path,$options=false) {
 		return pathinfo($path,$options);
+	}
+
+	public function realPath($path) {
+		return realpath($path);
+	}
+
+	public function tmpfile() {
+		return tmpfile();
 	}
 	
 }
